b20825696fde5e16292ae032f9a323068e94e1dd
Rafael Santos <109754987+rafaelduartejv@users.noreply.github.com>
2025-01-25 22:45:47 -0300

    Initial commit

---
+++ src/test/java/com/desafio/tokiomarine/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.tokiomarine.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
