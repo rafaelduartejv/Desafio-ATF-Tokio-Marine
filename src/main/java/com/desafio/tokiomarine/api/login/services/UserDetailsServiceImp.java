13e4e15678e3a28a32ceb04611c6a72fd9199101
Rafael Santos <109754987+rafaelduartejv@users.noreply.github.com>
2025-01-25 23:31:30 -0300

    feat: Adicionado a classe de Servico

---
+++ src/main/java/com/desafio/tokiomarine/api/login/services/UserDetailsServiceImp.java
@@ -0,0 +1,30 @@
+package com.desafio.tokiomarine.api.login.services;
+
+import com.desafio.tokiomarine.api.transferencia.models.Usuario;
+import com.desafio.tokiomarine.api.transferencia.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImp(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getSenha())
+                .roles("USER")
+                .build();
+    }
+}
